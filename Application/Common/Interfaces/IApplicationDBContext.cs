7ab68e488d4a2262853c32f98b43290b3dc08d23
DELL <DELL@DESKTOP-F884190>
2023-03-23 10:45:40 +0700

    Add project files.

---
+++ Application/Common/Interfaces/IApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDBContext
+    {
+            DbSet<AppSetting> AppSettings { get; set; }
+            Task<int> SaveChangesAsync();
+    }
+}
